51f8674188c5ad433607fc517f4f63a7f96b5c4c
ChrisMendes-11 <154396653+ChrisMendes-11@users.noreply.github.com>
2024-01-17 10:44:51 +0530

    Add files via upload

---
+++ add_2_nos.java
@@ -0,0 +1,21 @@
+package assignment;
+import java.util.Scanner;
+public class add_2_nos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("enter the first number");
+		int num1=scan.nextInt();
+		System.out.println("enter the second number");
+		int num2=scan.nextInt();
+		int sum=num1+num2;
+		System.out.println(sum);
+		
+
+
+	}
+
+}
